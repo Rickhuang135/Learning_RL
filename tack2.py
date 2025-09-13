4f4100b34faf6f066a65a5f291c41f52fc2b4a2e
rick <rickhuang135@gmail.com>
2025-09-14 10:16:33 +1200

    the bot is capable of learning the rules of the game, but it is inconsistent. Also, there seems to develop a situation where the bot can play one side very well while always losing when playing the other side.

--- tack2.py
+++ tack2.py
@@ -7,6 +7,7 @@ from random import randint
 from random import random
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+win_count = 0
 
 class TickTackToe:
     def __init__(self, state: torch.Tensor|None=None):
@@ -21,18 +22,20 @@ class TickTackToe:
     def __call__(self):
         return self.state.flatten()
 
-    def is_end(self) -> bool: # only check if the game has ended not who has won
+    def is_end(self) -> int: # 1 for win, 0 for draw, -1 for continue
         horizontal_sums = self.state.sum(1)
         if torch.max(torch.abs(horizontal_sums))==3:
-            return True
+            return 1
         vertical_sums = self.state.sum(0)
         if torch.max(torch.abs(vertical_sums))==3:
-            return True
+            return 1
         forward_slash = self.state[torch.arange(3), torch.arange(3)]
         backward_slash = self.state[torch.arange(3), [2,1,0]]
         if abs(torch.sum(forward_slash)) == 3 or abs(torch.sum(backward_slash))==3:
-            return True
-        return False
+            return 1
+        if len((self.state==0).nonzero()) == 0: # all squares are filled
+            return 0
+        return -1
     
     def is_legal(self, point: tuple) -> bool:
         if self.state[point[0],point[1]] != 0:
@@ -42,13 +45,21 @@ class TickTackToe:
     def move(self, point: tuple, player:int) -> int:
         if not self.is_legal(point):
             self.end = True
-            return -20
+            return 20
         self.state[point[0],point[1]]= player
-        if self.is_end():
-            self.end = True
-            return 0
-        else:
-            return -1
+    
+        is_end = self.is_end()
+        match is_end:
+            case -1: # not ended
+                return 1
+            case 0: # draw
+                self.end = True
+                return 5
+            case 1: # mover has won
+                self.end = True
+                return 0
+            case _:
+                raise Exception(f"unrecognised ending state '{is_end}'")
 
     def reset(self):
         self.state*=0
@@ -62,12 +73,15 @@ class DeepQModel(nn.Module): #inputs the current state of the board, outputs the
         self.a1 = nn.ReLU()
         self.l2 = nn.Linear(256, 256, dtype=float)
         self.a2 = nn.ReLU()
+        self.l3 = nn.Linear(256, 256, dtype=float)
+        self.a3 = nn.ReLU()
         self.lo = nn.Linear(256,9, dtype=float)
         self.ao = nn.ReLU()
 
     def forward(self, x):
         x = self.a1(self.l1(x))
         x = self.a2(self.l2(x))
+        # x = self.a3(self.l3(x))
         x = self.ao(self.lo(x))
         return x
 
@@ -76,14 +90,25 @@ def i2p(index) -> tuple:
         raise Exception(f"invalid index {index}")
     return index//3, index%3
 
-def max_a(Q: torch.Tensor) -> tuple:
-    index = torch.argmax(Q).item()
+def min_a(Q: torch.Tensor) -> tuple:
+    index = torch.argmin(Q).item()
     return i2p(index), index
 
 def rand_a() -> tuple:
     index = randint(0,8)
     return i2p(index), index
 
+def get_other_move(game: TickTackToe, model:DeepQModel):
+    # aquire Qs for moves of player
+    oQ = model(game()*-1)
+    o_min_a, o_min_a_ind = min_a(oQ)
+    # use best move if legal
+    if game.is_legal(o_min_a):
+        o_a = o_min_a
+    else:
+        matching_indices = (game.state == 0).nonzero(as_tuple=False)
+        o_a = tuple(matching_indices[randint(0, len(matching_indices)-1)])
+    return o_a
 
 state = TickTackToe()
 model = DeepQModel().to(device)
@@ -92,79 +117,105 @@ optimiser = optim.Adam(model.parameters(),lr=0.001)
 player = 1
 other = -1
 gamma = 0.8
-epsilon = 0.9
-episodes = 10
+epsilon = 1.0
+episodes = 10000
+
 interval = episodes//10
 step_size = epsilon/10
+if interval==0:
+    interval=1
 
-def back(s0, p_reward, p_max_a1):
+def back(s0: torch.Tensor, p_reward, p_min_a1, v=False):
     optimiser.zero_grad()
     pQ0 = model(s0)
     label = torch.clone(pQ0)
-    label[p_a0_ind] = p_reward + gamma*p_max_a1
-    print(f"s0: {s0}")
-    print(f"pQ0: {pQ0}")
-    print(f"label: {label}")
+    label[p_a0_ind] = p_reward + gamma*p_min_a1
     loss = criterion(pQ0, label)
     loss.backward()
+    if v:
+        print(f"s0: \n{s0.reshape(3,3)}")
+        print(f"pQ0: \n{pQ0.reshape(3,3)}")
+        print(f"label: \n{label.reshape(3,3)}")
+        print(f"loss: {loss}")
     optimiser.step()
     return loss.item()
 
 optimiser.zero_grad()
 with torch.no_grad():
     pQ0 = model(state())
-s0 = state()
+s0 = torch.clone(state())
+begin = True
 for i in range(episodes):
     runningloss = 0
+    total_reward = 0
     while not state.end:
-        p_max_a1 = 0
+        p_min_a1 = 0
         with torch.no_grad():
             # choose best move or random move
             if random() < epsilon:
                 p_a0, p_a0_ind = rand_a() # random move
             else:
-                p_a0, p_a0_ind = max_a(pQ0) # best move
+                p_a0, p_a0_ind = min_a(pQ0) # best move
             # make move and get reward
             p_reward = state.move(p_a0, player)
+            if p_reward == 0:
+                win_count+=1
             if not state.end: # player has not made illegal move or won
-                # aquire Qs for moves of other player
-                oQ = model(state() * -1)
-                o_max_a, o_max_a_ind = max_a(oQ)
-                # other player makes best legal move and get reward
-                if state.is_legal(o_max_a):
-                    o_a = o_max_a
-                else:
-                    matching_indices = (state.state == 0).nonzero(as_tuple=False)
-                    o_a = tuple(matching_indices[0])
+                o_a = get_other_move(state, model)
                 o_reward = state.move(o_a, other)
-                if state.end: # other has won
-                    p_reward=-10
+                if state.end: # other has won or drew
+                    if o_reward == 0: # other has won
+                        p_reward = 10
+                    else: # other has drawn
+                        p_reward = 2
                 else:
                     # get Q of best move for current player
                     pQ1 = model(state())
-                    p_max_a1 = torch.max(pQ1).item()
-        # update neural net work with reward + gamma * Q max a
-        runningloss+=back(s0, p_reward, p_max_a1)
+                    p_min_a1 = torch.min(pQ1).item()
+        # update neural net work with reward + gamma * Q min a
+        v = False
+        if episodes-i <= 1:
+            v=True
+        runningloss+=back(s0, p_reward, p_min_a1, v=v)
+        total_reward+=p_reward
         # change variables for next loop
         del pQ0
         del s0
-        s0 = state()
+        s0 = torch.clone(state())
         pQ0 = pQ1 #type: ignore
-    print(runningloss)
+    print(f"epsilon: {epsilon:1f} \t runningloss during episode: {runningloss:.5g} \t total_reward: {total_reward}")
     state.reset()
+    begin = not begin
+    if begin:
+        with torch.no_grad():
+            state.move(get_other_move(state,model), other)
+    s0 = torch.clone(state())
     if i % interval ==0:
         epsilon -= step_size
-        print(epsilon)
 
-print(state.state)
+print(f"win_count == {win_count}")
 
 def play():
     end = False
     game=TickTackToe()
-    while not end and not game.end:
-        print(game.move(max_a(model(game()))[0],1))
-        print(game.state)
-        move = input("")
-        a,b = move.split(",")
-        game.move((int(a),int(b)),-1)
-        
+    with torch.no_grad():
+        if random()<0.5:
+            while not end and not game.end:
+                bQs = model(game())
+                bot_move = i2p(int(torch.argmin(bQs)))
+                print(f" based on: \n{bQs.reshape(3,3)}")
+                print(f"bot made move {bot_move} and got reward {game.move(bot_move,1)}")
+                print(game.state)
+                move = input("")
+                a,b = move.split(",")
+                game.move((int(a),int(b)),-1)
+        else:
+            while not end and not game.end:
+                print(game.state)
+                move = input("")
+                a,b = move.split(",")
+                game.move((int(a),int(b)),-1)
+                bQs = model(game())
+                bot_move = i2p(int(torch.argmin(bQs)))
+                print(f" based on: \n{bQs.reshape(3,3)}")
+                print(f"bot made move {bot_move} and got reward {game.move(bot_move,1)}")
