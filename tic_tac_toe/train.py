fixed model loading mistake and policy gradient mistake

tic_tac_toe/train.py
@@ -17,7 +17,7 @@ class Train:
         self.model = A2CModel().to(device)
         self.hyper_params = hyper_params
         if "model_prefix" in hyper_params.keys():
-            model_paths, version = find_model(MODELPATH,hyper_params["model_prefix"])
+            model_paths, version = find_model(MODELPATH,hyper_params["model_prefix"], hyper_params["version"] if "version" in hyper_params.keys() else None)
             self.version = version
             for model_path in model_paths:
                 if "model" in model_path:
@@ -77,7 +77,7 @@ class Train:
         advantage = Vlabels - trainable_values.detach() # (n_parallel, replay_length - 1, n_symmetries)
         actions_expanded = augmented_actions[:,:].unsqueeze(-1) # (n_parallel, replay_length, n_symmetries) -> (n_parallel, replay_length-1 , n_symmetries, 1)  add nested layer to match log_prob dimensions 
         valid_log_prob = log_prob.gather(dim=3, index=actions_expanded).squeeze(-1) # (n_parallel, replay_length-1, n_symmetries)
-        grad_Pi = valid_log_prob*advantage*augmented_player_ids # (n_parallel, replay_length-1, n_symmetries)
+        grad_Pi = -1*valid_log_prob*advantage*augmented_player_ids # (n_parallel, replay_length-1, n_symmetries)
         Piloss = torch.sum(grad_Pi)
 
         # entropy normalisation

tic_tac_toe/ultils.py
@@ -21,7 +21,7 @@ def find_model(folder_path: str, model_prefix: str, version: list|None = None) -
                 version_str = c_version_str
         target_model = list(filter(lambda x: f"#{version_str}." in x, target_model))   
     else:
-        version_str = '_'.join(version)
+        version_str = '_'.join([str(x) for x in version])
         target_model = list(filter(lambda x: version_str in x, target_model_filter))
     if len(target_model)==0:
         raise Exception(f"Model {model_prefix} not found in {folder_path} ")
@@ -48,12 +48,12 @@ class SymmetryGenerator:
 
     def __init__(self, operations: list = [
         torch.clone,
-        lambda x: torch.flip(x, [1,0]),
-        torch.fliplr,
-        torch.flipud,
-        lambda x: torch.transpose(x, 1, 0),
-        torch.rot90,
-        lambda x: torch.rot90(x, 3),
+        # lambda x: torch.flip(x, [1,0]),
+        # torch.fliplr,
+        # torch.flipud,
+        # lambda x: torch.transpose(x, 1, 0),
+        # torch.rot90,
+        # lambda x: torch.rot90(x, 3),
     ]):
         self.n_ops = len(operations)
         inds = torch.arange(9, dtype=torch.int32, device=device).reshape((3,3))